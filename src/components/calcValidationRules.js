82b564b88ee5b13c9a31d7bf037c72027f850c89
Kfoster14 <kelleyafoster@hotmail.com>
2021-07-07 14:46:43 +1000

    added units to results

--- src/components/calcValidationRules.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function validate (values) {
-    let errors = {}; 
-    if (!values.dailyUsage) {
-        errors.dailyUsage = 'Number is required';
-      }
-    return errors;
-}
